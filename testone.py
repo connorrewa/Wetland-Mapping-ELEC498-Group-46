d122381c8fb17e859aecc9c605e2bb879b53592a
Jacob <jacobbadali5@gmail.com>
2026-01-08 12:11:51 -0500

    checking new download

--- testone.py
+++ testone.py
@@ -1,7 +1,7 @@
 import rasterio
 import numpy as np
 
-with rasterio.open('bow_river_embeddings_2020_CORRECTED-0000021504-0000009216.tif') as src:
+with rasterio.open('bow_river_embeddings_2020_CORRECTED-0000021504-0000030720.tif') as src:
     data = src.read(1, window=((0, 10), (0, 10)))
     print("Sample data:", data[:5, :5])
     print("All NaN?:", np.all(np.isnan(data)))
