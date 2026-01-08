1e4bd2ef75e25533b00a3d844170cdf2e330831a
Jacob <jacobbadali5@gmail.com>
2026-01-08 10:48:52 -0500

    class distribution and testing one google earth download

--- testone.py
+++ testone.py
@@ -1,7 +1,7 @@
 import rasterio
 import numpy as np
 
-with rasterio.open('bow_river_embeddings_2020_matched-0000006144-0000006144.tif') as src:
+with rasterio.open('bow_river_embeddings_2020_CORRECTED-0000000000-0000018432.tif') as src:
     data = src.read(1, window=((0, 10), (0, 10)))
     print("Sample data:", data[:5, :5])
     print("All NaN?:", np.all(np.isnan(data)))
